b558c5da96c2e28b9230e029c903343df5bba88a
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-22 22:18:52 +0100

    Allowed CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import create_db_and_tables
 from .routers import user, verification_code, friend_request
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(verification_code.router)
