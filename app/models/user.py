5b1f949c517724b740d312e54877cbeb7751bbab
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-18 13:37:05 +0100

    Better structured and cleaned User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,30 +1,18 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime, timezone
-
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from ..models.friend_request import FriendRequest
 
 class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
     display_name: str = Field(index=True, unique=False)
     email: str = Field(index=True, unique=True)
     phone_number: str = Field(index=True, unique=True)
-    password: str
-    profile_picture: str = Field(nullable=True)
+    password: Optional[str] = Field(nullable=True)
+    profile_picture: Optional[str] = Field(nullable=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 class User(UserBase, table=True):
     user_id: Optional[int] = Field(default=None, primary_key=True)
-
-    # friend_requests_sent: list["FriendRequest"] = Relationship(
-    #     sa_relationship_kwargs={"foreign_keys": "FriendRequest.sender_id"}
-    # )
-    # friend_requests_received: list["FriendRequest"] = Relationship(
-    #     sa_relationship_kwargs={"foreign_keys": "FriendRequest.receiver_id"}
-    # )
-    # friends: list["Friend"] = Relationship(back_populates="user")
 
 class UserCreate(UserBase):
     pass
