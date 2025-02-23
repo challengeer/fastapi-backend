8ddea6db56efcfa4a8557812191d302dae44f300
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-23 21:38:36 +0100

    Adedd both side look to search

--- app/routers/user.py
+++ app/routers/user.py
@@ -40,7 +40,8 @@ def search_users(
         )
         .outerjoin(
             FriendRequest,
-            (FriendRequest.receiver_id == User.user_id) & (FriendRequest.sender_id == current_user_id)
+            (FriendRequest.receiver_id == User.user_id) & (FriendRequest.sender_id == current_user_id) |
+            (FriendRequest.receiver_id == current_user_id) & (FriendRequest.sender_id == User.user_id)
         )
         .where(
             (User.display_name.like(f"%{q}%")) | (User.username.like(f"%{q}%"))
